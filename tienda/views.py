62ff6add19a3b00a7859152a72c8b28117082777
GTXs <arayabarrioseduardo@gmail.com>
2021-02-01 09:24:07 -0300

    pequeña correcion busqueda

--- tienda/views.py
+++ tienda/views.py
@@ -16,4 +16,16 @@ def search(request):
     text = request.GET.get('text', '') # Obtenemos el texto mediante GET
     text = text.upper() # Ponemos el texto en mayusculas
     products = Product.objects.filter(name__contains=text)
+    if not products:
+        # Si no encontro el producto con mayuscula, prueba con minusculas
+        text = text.lower()
+        products = Product.objects.filter(name__contains=text)
+    if not products:
+        # Si no encontro el producto con minusculas, prueba capitalizar
+        text = text.capitalize()
+        products = Product.objects.filter(name__contains=text)
+    if not products:
+        # Si no encontro el producto con capitalizar, prueba title
+        text = text.title()
+        products = Product.objects.filter(name__contains=text)  
     return render(request, 'tienda/search.html', {'products':products})
