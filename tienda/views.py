405e55f2935d1a905485f1323a006e224b750bf7
GTXs <arayabarrioseduardo@gmail.com>
2021-02-01 08:37:30 -0300

    añadiendo buscador

--- tienda/views.py
+++ tienda/views.py
@@ -1,9 +1,19 @@
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from .models import Product
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 class Home(ListView):
     model = Product
-    paginate_by = 6
+    paginate_by = 12
+
+def search(request):
+    """
+    Obtenemos mediante get el producto que 
+    se esta buscando
+    """
+    text = request.GET.get('text', '') # Obtenemos el texto mediante GET
+    text = text.upper() # Ponemos el texto en mayusculas
+    products = Product.objects.filter(name__contains=text)
+    return render(request, 'tienda/search.html', {'products':products})
