56e220c0a7695b45f719a6c046fb8a1fccbf97f0
GTXs <arayabarrioseduardo@gmail.com>
2021-03-17 17:58:36 -0300

    busqueda case insensitive

--- tienda/views.py
+++ tienda/views.py
@@ -14,18 +14,5 @@ def search(request):
     se esta buscando
     """
     text = request.GET.get('text', '') # Obtenemos el texto mediante GET
-    text = text.upper() # Ponemos el texto en mayusculas
-    products = Product.objects.filter(name__contains=text)
-    if not products:
-        # Si no encontro el producto con mayuscula, prueba con minusculas
-        text = text.lower()
-        products = Product.objects.filter(name__contains=text)
-    if not products:
-        # Si no encontro el producto con minusculas, prueba capitalizar
-        text = text.capitalize()
-        products = Product.objects.filter(name__contains=text)
-    if not products:
-        # Si no encontro el producto con capitalizar, prueba title
-        text = text.title()
-        products = Product.objects.filter(name__contains=text)  
+    products = Product.objects.filter(name__icontains=text) 
     return render(request, 'tienda/search.html', {'products':products})
