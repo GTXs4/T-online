420669034220b14d3446a5ba4cb29b50b57f1a12
GTXs <arayabarrioseduardo@gmail.com>
2021-03-18 18:44:41 -0300

    Avance busqueda asincrona

--- tienda/urls.py
+++ tienda/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('search/', views.search, name="search"),
+    path('search/asyn/', views.search_asynchronous, name="search_asyn"),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -2,6 +2,8 @@ from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from .models import Product
 from django.shortcuts import render, redirect
+from django.core.paginator import Paginator
+from django.http import JsonResponse
 
 # Create your views here.
 class Home(ListView):
@@ -14,5 +16,44 @@ def search(request):
     se esta buscando
     """
     text = request.GET.get('text', '') # Obtenemos el texto mediante GET
-    products = Product.objects.filter(name__icontains=text) 
-    return render(request, 'tienda/search.html', {'products':products})
+    page_num = int(request.GET.get('page', 1))
+    products = Product.objects.filter(name__icontains=text)
+    product_paginator = Paginator(products, 12)
+    page = product_paginator.get_page(page_num)
+    context = {
+        'text' : text,
+        'page' : page
+        }
+    return render(request, 'tienda/search.html', context)
+
+def search_asynchronous(request):
+    text = request.GET.get('text', '')
+    products = Product.objects.filter(name__icontains=text)
+    product_paginator = Paginator(products, 12)
+    page = product_paginator.get_page(1)
+
+    # Crea una lista de diccionarios con los productos
+    page_products = []
+    for product in page.object_list:
+        page_products.append({
+            'name' : product.name, 
+            'price' : product.price, 
+            'url' : product.url_image.url[1:] if product.url_image else None
+            })
+    # Asigna las pagina previa y la siguiente
+    try:
+        next_page = page.next_page_number()
+    except:
+        next_page = None
+    try:
+        previous_page = page.previous_page_number()
+    except:
+        previous_page = None 
+    
+    json_response = {
+        'text' : text,
+        'next_page' : next_page,
+        'previous_page' : previous_page, 
+        'page_products' : page_products        
+        }
+    return JsonResponse(json_response, safe=False)
