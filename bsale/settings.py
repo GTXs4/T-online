ee11c3545aaa1c96833ee469d907d77bdec8732b
GTXs <arayabarrioseduardo@gmail.com>
2021-03-22 17:58:02 -0300

    configuraciones djangorestframework

--- bsale/settings.py
+++ bsale/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'rest_framework',
     'tienda',
 ]
 
@@ -111,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
