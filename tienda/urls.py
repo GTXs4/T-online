fb752c9f7753ac11bd0123aba07c8874e99ad80d
GTXs <arayabarrioseduardo@gmail.com>
2021-03-23 00:36:43 -0300

    Rest Api Products

--- tienda/urls.py
+++ tienda/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register('products', views.ProductViewSet)
 
 urlpatterns = [
     path('', views.Home.as_view(), name="home"),
+    path('api/v1/', include(router.urls)),
     path('search/', views.search, name="search"),
     path('search/asyn/', views.search_asynchronous, name="search_asyn"),
 ]
--- tienda/views.py
+++ tienda/views.py
@@ -1,6 +1,11 @@
 from django.views.generic.base import TemplateView
 from django.views.generic.list import ListView
 from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import viewsets, filters
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
+
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator
 from django.http import JsonResponse
@@ -9,6 +14,26 @@ from django.http import JsonResponse
 class Home(ListView):
     model = Product
     paginate_by = 12
+
+class ProductViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    # Paginacion
+    pagination_class = PageNumberPagination
+    pagination_class.page_size = 12
+
+    # Filtros
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter, DjangoFilterBackend]
+    # Campos para la busqueda
+    search_fields = ['name']
+    # Campos para la ordenacion
+    ordering_fields = ['name', 'price', 'category']
+    # Campos para los filtros
+    filterset_fields = {
+        'price': ['lte', 'gte'],
+        'category': ['exact']
+    }
 
 def search(request):
     """
