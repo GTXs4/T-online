f8c46124dfdae01e98071cb20645107c881655a7
GTXs <arayabarrioseduardo@gmail.com>
2021-03-23 13:09:17 -0300

    Custom Field Serializer

--- tienda/serializers.py
+++ tienda/serializers.py
@@ -1,7 +1,15 @@
 from .models import Product
 from rest_framework import serializers
+from django.contrib.humanize.templatetags.humanize import intcomma
 
 class ProductSerializer(serializers.ModelSerializer):
+
+    url_image = serializers.ImageField(use_url=False)
+    price = serializers.SerializerMethodField('get_price_formatted')
+
     class Meta:
         model = Product
         fields = '__all__'
+
+    def get_price_formatted(self, product):
+        return intcomma(product.price)
